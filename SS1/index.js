1b3819bfaf85b6bfca4c48386f5e851b48b5399b
LongNTDev <codeinlife10@example.com>
2023-11-05 10:49:27 +0700

    DOM

--- SS1/index.js
+++ SS1/index.js
@@ -1,217 +1,217 @@
-// Code javascript trong file mới
+// // Code javascript trong file mới
 
-// Biến trong javascript
-// let/var/const
+// // Biến trong javascript
+// // let/var/const
 
-// Khác biệt giữa let và const 
-let str1 = "Đây là buổi học đầu tiên của CIJS01" // Kiểu dữ liệu là 1 string
-let str2 = 'Đây là ví dụ 2 về khai báo string'
-let str3 = `${str1} + ${str2}` // template string
-// str3 = "Đây là buổi học đầu tiên của CIJS01 + Đây là ví dụ 2 về khai báo string"
-let str4 = str1 + " + " + str2
-let str5 = ""
-let num = 5 // Kiểu dữ liệu là 1 number
+// // Khác biệt giữa let và const 
+// let str1 = "Đây là buổi học đầu tiên của CIJS01" // Kiểu dữ liệu là 1 string
+// let str2 = 'Đây là ví dụ 2 về khai báo string'
+// let str3 = `${str1} + ${str2}` // template string
+// // str3 = "Đây là buổi học đầu tiên của CIJS01 + Đây là ví dụ 2 về khai báo string"
+// let str4 = str1 + " + " + str2
+// let str5 = ""
+// let num = 5 // Kiểu dữ liệu là 1 number
 
-let isLogin = true // boolean: true/false -> Trong câu lệnh điều kiện if...else
+// let isLogin = true // boolean: true/false -> Trong câu lệnh điều kiện if...else
 
-let example1 // undefined - Giá trị chưa được định nghĩa
-let example2 = null // Mang giá trị nhưng giá trị không có gì 
+// let example1 // undefined - Giá trị chưa được định nghĩa
+// let example2 = null // Mang giá trị nhưng giá trị không có gì 
 
-// array: CRUD - Create, Read, Update, Delete
+// // array: CRUD - Create, Read, Update, Delete
 
-// create
-let nameArr = ["Tên anh là Nguyễn Thành Long", "Tên học sinh là Đinh Tuấn Khang"]
+// // create
+// let nameArr = ["Tên anh là Nguyễn Thành Long", "Tên học sinh là Đinh Tuấn Khang"]
 
-// read: Đọc dựa trên index= 0 - nameArr.length - 1  
-console.log(nameArr[1])
-
-
-// update
-nameArr[0] = "Không phải Nguyễn Thành Long đâu"
-
-console.log(nameArr[0])
-
-nameArr.push("Hôm nay trời đẹp quá") // method dùng để thêm 1 phần tử vào cuối mảng
-
-console.log(nameArr)
-
-nameArr.pop() // method dùng để lấy 1 giá trị ở cuối mảng ra 
-
-console.log(nameArr)
-
-nameArr.shift() // method dùng để lấy 1 giá trị ở đầu mảng ra  
-
-console.log(nameArr)
-
-nameArr.unshift("Thêm 1 phần tử mới") // method dùng để thêm 1 phần tử vào đầu mảng
-
-console.log(nameArr)
-
-// Thêm mới/xóa các phần tử ở vị trị khác trong mảng 
-let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-// slice/splice
+// // read: Đọc dựa trên index= 0 - nameArr.length - 1  
+// console.log(nameArr[1])
 
 
-// slice sẽ tạo ra 1 mảng mới từ mảng ban đầu 
-let sliceArr = numArr.slice(3, 8) // start-<end
+// // update
+// nameArr[0] = "Không phải Nguyễn Thành Long đâu"
 
-console.log("Đây là mảng sau khi dùng slice", sliceArr)
-console.log("Đây là mảng ban đầu", numArr)
+// console.log(nameArr[0])
 
-// splice tác động trực tiếp vào mảng ban đầu 
-// let spliceArr = numArr.splice(0, 2)
+// nameArr.push("Hôm nay trời đẹp quá") // method dùng để thêm 1 phần tử vào cuối mảng
+
+// console.log(nameArr)
+
+// nameArr.pop() // method dùng để lấy 1 giá trị ở cuối mảng ra 
+
+// console.log(nameArr)
+
+// nameArr.shift() // method dùng để lấy 1 giá trị ở đầu mảng ra  
+
+// console.log(nameArr)
+
+// nameArr.unshift("Thêm 1 phần tử mới") // method dùng để thêm 1 phần tử vào đầu mảng
+
+// console.log(nameArr)
+
+// // Thêm mới/xóa các phần tử ở vị trị khác trong mảng 
+// let numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// // slice/splice
+
+
+// // slice sẽ tạo ra 1 mảng mới từ mảng ban đầu 
+// let sliceArr = numArr.slice(3, 8) // start-<end
+
+// console.log("Đây là mảng sau khi dùng slice", sliceArr)
+// console.log("Đây là mảng ban đầu", numArr)
+
+// // splice tác động trực tiếp vào mảng ban đầu 
+// // let spliceArr = numArr.splice(0, 2)
+
+// // console.log("Đây là mảng sau khi dùng splice", spliceArr)
+// // console.log("Đây là mảng ban đầu", numArr)
+
+// let spliceArr = numArr.splice(3, 3, 15, 16)
 
 // console.log("Đây là mảng sau khi dùng splice", spliceArr)
 // console.log("Đây là mảng ban đầu", numArr)
 
-let spliceArr = numArr.splice(3, 3, 15, 16)
+// // object: CRUD
+// // properties
+// // methods
 
-console.log("Đây là mảng sau khi dùng splice", spliceArr)
-console.log("Đây là mảng ban đầu", numArr)
+// // create
+// let user = {
+//     name: "Đinh Phúc Khang",
+//     age: 17,
+//     favorites: ["Chơi game", "Học computer science"],
 
-// object: CRUD
-// properties
-// methods
+//     register: function () {
 
-// create
-let user = {
-    name: "Đinh Phúc Khang",
-    age: 17,
-    favorites: ["Chơi game", "Học computer science"],
+//     },
+//     login: function () {
 
-    register: function () {
+//     },
+//     printSum: function () {
 
-    },
-    login: function () {
+//     }
+// }
 
-    },
-    printSum: function () {
+// // read
+// console.log(user.name)
+// console.log(user.age)
 
-    }
-}
+// // update
+// user.name = "Nguyễn Thành Long"
 
-// read
-console.log(user.name)
-console.log(user.age)
+// console.log(user)
 
-// update
-user.name = "Nguyễn Thành Long"
+// // delete 
+// delete user.name
 
-console.log(user)
-
-// delete 
-delete user.name
-
-console.log(user)
+// console.log(user)
 
 
-// Tham chiếu/Tham trị
+// // Tham chiếu/Tham trị
 
-// Trường hợp truyền tham trị: Kiểu dữ liệu nguyên thủy number, string, boolean
-// let exam1 = 5
-// let exam2 = exam1
+// // Trường hợp truyền tham trị: Kiểu dữ liệu nguyên thủy number, string, boolean
+// // let exam1 = 5
+// // let exam2 = exam1
 
-// exam1 = 10
+// // exam1 = 10
 
-// console.log("Đây là giá trị của exam1", exam1)
-// console.log("Đây là giá trị của exam2", exam2)
+// // console.log("Đây là giá trị của exam1", exam1)
+// // console.log("Đây là giá trị của exam2", exam2)
 
-// Trường hợp truyền tham chiếu: Kiểu dữ liệu là array, object
-// let exam1 = [1,2,3,4]
-// let exam2 = exam1
+// // Trường hợp truyền tham chiếu: Kiểu dữ liệu là array, object
+// // let exam1 = [1,2,3,4]
+// // let exam2 = exam1
 
-// exam2.push(5)
+// // exam2.push(5)
 
-// console.log("Đây là giá trị của exam1", exam1)
-// console.log("Đây là giá trị của exam2", exam2)
+// // console.log("Đây là giá trị của exam1", exam1)
+// // console.log("Đây là giá trị của exam2", exam2)
 
-// Chuyển việc truyền tham chiếu thành tham trị khi sử dụng array/object
-// let exam1 = [1,2,3,4]
+// // Chuyển việc truyền tham chiếu thành tham trị khi sử dụng array/object
+// // let exam1 = [1,2,3,4]
+// // // spread operator ...
+// // // let exam2 = [ ...exam1 ]
+// // let exam2 = exam1.slice(0)
+
+// // exam1.push(5)
+
+// // console.log("Đây là giá trị của exam1", exam1)
+// // console.log("Đây là giá trị của exam2", exam2)
+
+// let exam1 = {
+//     name: "Đinh Phúc Khang",
+//     age: 17,
+//     favorites: ["Chơi game", "Học computer science"],
+// }
+
+// let exam2 = {
+//     name: "Nguyễn Thành Long",
+//     state: "Texas"
+// }
+
 // // spread operator ...
-// // let exam2 = [ ...exam1 ]
-// let exam2 = exam1.slice(0)
+// // let exam2 = { ...exam1 }
+// let exam3 = Object.assign({}, exam2, exam1)
 
-// exam1.push(5)
+// console.log("Đây là exam1", exam1)
+// console.log("Đây là exam2", exam2)
+// console.log("Đây là exam3", exam3)
 
-// console.log("Đây là giá trị của exam1", exam1)
-// console.log("Đây là giá trị của exam2", exam2)
+// // exam1.newKey = "test"
 
-let exam1 = {
-    name: "Đinh Phúc Khang",
-    age: 17,
-    favorites: ["Chơi game", "Học computer science"],
-}
+// // console.log("Đây là giá trị của exam1", exam1)
+// // console.log("Đây là giá trị của exam2", exam2)
 
-let exam2 = {
-    name: "Nguyễn Thành Long",
-    state: "Texas"
-}
+// // Câu lệnh điều kiện: if, else if, else
+// // if(biểu thức logic) {
 
-// spread operator ...
-// let exam2 = { ...exam1 }
-let exam3 = Object.assign({}, exam2, exam1)
+// // }
 
-console.log("Đây là exam1", exam1)
-console.log("Đây là exam2", exam2)
-console.log("Đây là exam3", exam3)
+// // Toán tử logic: <, >, >=, <=, ==,, ===, !=, &&, ||
+// // 5 < 5 -> false
+// // 5 <= 5 => true
 
-// exam1.newKey = "test"
+// // let inputUser = prompt("Nhập vào 1 ngày để kiểm tra là ngày nghỉ hay đi làm")
 
-// console.log("Đây là giá trị của exam1", exam1)
-// console.log("Đây là giá trị của exam2", exam2)
+// // if(inputUser > 0) {
+// //     console.log(`${inputUser} là số dương`)
+// // } else if(inputUser < 0) {
+// //     console.log(`${inputUser} là số âm`)
+// // } else if(inputUser == 0){ 
+// //     console.log(`${inputUser} là số 0`)
+// // } else { // Các trường hợp còn lại
+// //     console.log("Có lỗi xảy ra, kiểm tra lại")
+// // }
 
-// Câu lệnh điều kiện: if, else if, else
-// if(biểu thức logic) {
+// // switch(inputUser) {
+// //     case "Monday":
+// //         console.log("Hôm nay là ngày đi làm")
+// //         break
+// //     case "Tuesday": 
+// //         console.log("Hôm nay là ngày đi làm")
+// //         break
+// //     case "Sunday":
+// //         console.log("Hôm nay là ngày nghỉ")
+// //         break
+// //     default: 
+// //         console.log("Ngày nhập vào k hợp lệ")
+// //         break
+// // }
 
-// }
+// // Vòng lặp for  
 
-// Toán tử logic: <, >, >=, <=, ==,, ===, !=, &&, ||
-// 5 < 5 -> false
-// 5 <= 5 => true
+// // for(let i = 0; i< 50; i++){
+// //     console.log(i)
+// // }
 
-// let inputUser = prompt("Nhập vào 1 ngày để kiểm tra là ngày nghỉ hay đi làm")
+// // let newArr = [1,2,3,4,5,6,7,8,9,10]
 
-// if(inputUser > 0) {
-//     console.log(`${inputUser} là số dương`)
-// } else if(inputUser < 0) {
-//     console.log(`${inputUser} là số âm`)
-// } else if(inputUser == 0){ 
-//     console.log(`${inputUser} là số 0`)
-// } else { // Các trường hợp còn lại
-//     console.log("Có lỗi xảy ra, kiểm tra lại")
-// }
+// // // console.log(newArr[0])
+// // // console.log(newArr[1])
+// // // console.log(newArr[2])
 
-// switch(inputUser) {
-//     case "Monday":
-//         console.log("Hôm nay là ngày đi làm")
-//         break
-//     case "Tuesday": 
-//         console.log("Hôm nay là ngày đi làm")
-//         break
-//     case "Sunday":
-//         console.log("Hôm nay là ngày nghỉ")
-//         break
-//     default: 
-//         console.log("Ngày nhập vào k hợp lệ")
-//         break
-// }
-
-// Vòng lặp for  
-
-// for(let i = 0; i< 50; i++){
-//     console.log(i)
-// }
-
-// let newArr = [1,2,3,4,5,6,7,8,9,10]
-
-// // console.log(newArr[0])
-// // console.log(newArr[1])
-// // console.log(newArr[2])
-
-// for(let i = 0; i < newArr.length; i++){
-//     newArr[i] = newArr[i] + 5 
-// }
+// // for(let i = 0; i < newArr.length; i++){
+// //     newArr[i] = newArr[i] + 5 
+// // }
 
 
 // 1. Khai báo 1 mảng [91,26,4,6,5,85,7,11,9,22] và in ra chỉ số (index) tại vị trí đó giá trị của nó là số chẵn 
@@ -219,3 +219,42 @@ console.log("Đây là exam3", exam3)
 // 3. Cho 1 mảng users = [ {username: "admin", password: "123456"}, {username: "user1", password: "testpassword"}, { username: "manager", password: "dinhtuankhang" } ]. Người dùng nhập vào 2 giá trị username và password, kiểm tra xem thông tin người dùng nhập vào có tồn tại trong hệ thống k. Nếu có thông báo thành công, sai thì thông báo thất bại
 // 4. Làm 1 chức năng đăng ký cho phép người dùng tạo tài khoản trong hệ thống (mật khẩu ít nhất 8 ký tự)
 
+// 1
+let arr = [91, 26, 4, 6, 5, 85, 7, 11, 9, 22]
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(i)
+    }
+}
+
+
+// 2
+let num = []
+for (let i = 0; i < 1000000; i++) {
+    num.push(i)
+}
+console.log(num)
+
+// 3.
+let users = [{ username: "admin", password: "123456" },
+{ username: "user1", password: "testpassword" },
+{ username: "manager", password: "dinhtuankhang" }]
+let nameCheck = "admin"
+let passCheck = "123456"
+let check = false;
+for (let i = 0; i < users.length; i++) {
+    if (users[i].username == nameCheck && user[i].password == passCheck)
+        console.log("dang nhap thanh cong")
+    check = true
+}
+if (!check) {
+    console.log("dang nhap that bai")
+}
+
+// 4.
+let newUser = {
+    usename: "",
+    password: "",
+}
+users.push(newUser)
+console.log(users)
---
+++ SS2/index.js
@@ -0,0 +1,80 @@
+// DOM: document object model  
+// DOM: Làm cho web động, dữ liệu có thể thay đổi theo thời gian hoặc là theo tương tác của người dùng. Thay đổi style css động 
+console.dir(document)
+
+// Có 3 cách để tìm 1 thẻ trong html 
+// Cách 1: Lấy thông qua id 
+const content = document.getElementById('content')
+const username = document.getElementById('username')
+console.log(content)
+
+// Thay đổi nội dung của thẻ 
+
+// Thay đổi text bên trong
+// content.innerText = "Đây là buổi học thứ 2"
+
+// Thay đổi html bên trong thẻ
+// content.innerHTML = "<a href='https://www.youtube.com/'>Bấm để chuyển sang youtube</a>"
+
+// Thay đổi style css
+content.style.color = "blue" 
+content.style.backgroundColor = "red" 
+
+// Các sự kiện: Xảy ra khi người dùng thực hiện tương tác trên trang web 
+// Nó xảy ra ở đâu, khi xảy ra sự kiện thì cần làm gì? 
+function handleClick() {
+    console.log("Bạn mới bấm thẻ này")
+}
+
+function handleChange() {
+    console.log("Thẻ HTML mới bị thay đổi")
+}
+
+function handleKeydown() {
+    console.log("Người dùng mới gõ phím")
+}
+
+// content.addEventListener("click", handleClick)
+
+content.addEventListener("click", (e) => {
+    console.log(e)
+})
+
+username.addEventListener("change", (e) => {
+    // e.target.name dùng để lấy ra giá trị từ thẻ input
+    console.log(e.target.value)
+})
+
+
+// username.addEventListener("keydown", handleKeydown)
+
+// username.addEventListener("keydown", (event) => {
+//     console.log(event)
+// })
+
+// Callback function: Hàm tham số 1 hàm khác 
+// keydown chạy -> return object (Chứa các thông tin của sự kiện đó)
+
+
+// Lấy thẻ thông qua className
+// Luôn trả về 1 mảng 
+// let tagEl = document.getElementsByClassName("get-by-class")
+
+
+// for(let i = 0; i< tagEl.length; i++) {
+//     tagEl[i].addEventListener("click", function () {
+//         console.log(tagEl[i].innerText)
+//     })
+// }
+// console.log(tagEl)
+
+
+// 1. Tạo ra 4 thẻ input cho phép người dùng nhập username, password, fullname, email
+// 2. Lấy ra các giá trị được nhập vào bởi người dùng theo 2 cách 
+        // 2.1. Khi người dùng bấm nút trên html 
+        // 2.2. Khi người dùng gõ enter 
+// 3. Sau khi lấy ra được giá trị thì thực hiện kiểm tra với các yêu cầu sau
+        // 3.1. Các thông tin không được để trống
+        // 3.2. Password > 6, < 20 
+        // 3.3. Email có chứa dấu @ 
+    // Nếu như người dùng vi phạm cần thông lỗi cho người dùng biết
